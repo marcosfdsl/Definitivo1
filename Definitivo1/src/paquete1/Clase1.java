a6a9a3e7e2e23d498490a67f3cf8e190bdaf0f4c
Pablo Sastre <pablosastre@live.u-tad.com>
2022-12-21 12:16:26 +0100

    f1 1

--- Definitivo1/src/paquete1/Clase1.java
+++ Definitivo1/src/paquete1/Clase1.java
@@ -1,11 +1,23 @@
 package paquete1;
 
+import java.util.Scanner;
+
 public class Clase1 {
 
 	public static void main(String[] args) {
-
-		System.out.println("Hola Mundo");
-
+		Scanner sc = new Scanner(System.in);
+		
+		String string1 = "Entornos de desarrollo es la mejor asignatura";
+		String string2 = "Bases de datos es muy entretenida";
+		
+		System.out.println("Cuantas veces quieres que se repitan: ");
+		int rep = sc.nextInt();
+		
+		System.out.println("\nRepitiendo... \n");
+		for(int i=0;i<rep;i++) {
+			System.out.println(string1);
+			System.out.println(string2);
+		}
 	}
 
 }
