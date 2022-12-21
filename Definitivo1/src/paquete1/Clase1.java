7cdf6a2d2f631416aad01409b3eed59504e1c658
Luis Barrado <luis.barrado@live.u-tad.com>
2022-12-21 12:15:07 +0100

    F4 1

--- Definitivo1/src/paquete1/Clase1.java
+++ Definitivo1/src/paquete1/Clase1.java
@@ -1,11 +1,21 @@
 package paquete1;
 
+import java.util.Scanner;
+
 public class Clase1 {
 
 	public static void main(String[] args) {
-
-		System.out.println("Hola Mundo");
-
+		
+		Scanner src= new Scanner(System.in);
+		System.out.println("Introduce un numero factorial ");
+		int numero=src.nextInt();
+		int factorial=1;
+		
+		for (int i=2;i<=numero;i++) {
+		factorial=factorial * i;	
+		}
+	System.out.println("El factorial de un número "+Integer.toString(numero)+ " es " +Integer.toString(factorial));
 	}
 
-}
+
+	}
