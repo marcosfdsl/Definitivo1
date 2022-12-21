1421a0a0ea9318be76b2d6b7d04d76ee6506fcd2
DCK <alegar22@gmail.com>
2022-12-21 12:14:41 +0100

    f5 1

--- Definitivo1/src/paquete1/Clase1.java
+++ Definitivo1/src/paquete1/Clase1.java
@@ -1,10 +1,26 @@
 package paquete1;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Clase1 {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hola Mundo");
+		int[] numbers = new int[10];
+	    Random random = new Random();
+	    for (int i = 0; i < numbers.length; i++) {
+	      numbers[i] = random.nextInt(100);
+	    }
+
+	    //ARRAY SIN ORDENAR
+	    System.out.println("Array antes de ordenar: " + Arrays.toString(numbers));
+
+	    // ORDENAMOS EL ARRAY DE MANOR A MAYOR
+	    Arrays.sort(numbers);
+
+	    //ARRAY ORDENADO
+	    System.out.println("Array después de ordenar: " + Arrays.toString(numbers));
 
 	}
 
