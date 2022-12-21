f699d50cdea5ba233bbf40c296e10c6c792d76fa
MarcosFdS <marcos.fernandez@live.u-tad.com>
2022-12-21 12:41:20 +0100

    commit 2 revision

--- Definitivo1/src/paquete1/Clase1.java
+++ Definitivo1/src/paquete1/Clase1.java
@@ -7,14 +7,14 @@ public class Clase1 {
 	public static void main(String[] args) {
 		
 		Scanner src= new Scanner(System.in);
-		System.out.println("Introduce un numero factorial ");
+		System.out.println("Introduce un numero");
 		int numero=src.nextInt();
 		int factorial=1;
 		
 		for (int i=2;i<=numero;i++) {
 		factorial=factorial * i;	
 		}
-	System.out.println("El factorial de un número "+Integer.toString(numero)+ " es " +Integer.toString(factorial));
+	System.out.println("El factorial del número "+Integer.toString(numero)+ " es " +Integer.toString(factorial));
 	}
 
 
