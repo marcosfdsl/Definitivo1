b1bbd60e330d17f47137e80e059f00d6e90b8c35
Iván Díez C. <ivandiezcardenas@gmail.com>
2022-12-21 13:22:35 +0100

    f6 1

--- Definitivo1/src/paquete1/Clase1.java
+++ Definitivo1/src/paquete1/Clase1.java
@@ -1,11 +1,39 @@
 package paquete1;
 
+import java.util.Scanner;
+
 public class Clase1 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Hola Mundo");
+		System.out.println();
 
-	}
+		System.out.println("Escribe una frase");
 
+		String frase = sc.nextLine();
+
+		System.out.println("Escribe una letra");
+
+		char letra = sc.next().charAt(0);
+
+		System.out.println("La letra " + letra + " se repite " + conteoLetra(frase, letra) + " veces.");
+
+		}
+
+		public static int conteoLetra(String frase, char letra) {
+
+		int contador = 0;
+		
+		for (char l : frase.toCharArray()) {
+
+		if(l == letra)
+
+		contador++;
+
+		}
+
+		return contador;
+
+		}
 }
