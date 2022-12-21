3d4cf1e85254edb0ef6846a00ffb020f571af30a
MarcosFdS <marcos.fernandez@live.u-tad.com>
2022-12-21 14:15:46 +0100

    C6

--- Definitivo1/src/paquete1/Clase1.java
+++ Definitivo1/src/paquete1/Clase1.java
@@ -51,4 +51,48 @@ public class Clase1 {
 		return contador;
 
 		}
+	
+	public static Scanner entrada = new Scanner(System.in);
+	
+	public static double pedirDatos(double segundos) {
+		
+		System.out.println("Introduce un número entero");
+		
+		segundos = entrada.nextInt();
+		
+		return segundos;
+		
+	}
+	
+	public static void pasarAHoras (double segundos, double segundosAux) {
+		
+		double minutos = 0;
+		
+		double horas = 0;
+		
+		while (segundos >= 60) {
+			
+			if (segundos >= 60) {
+			
+				segundos = segundos - 60;
+				
+				minutos++;
+			
+			}
+		}
+		
+		while (minutos >= 60) {
+		
+			if (minutos >= 60) {
+			
+				minutos = minutos - 60;
+				
+				horas++;
+			
+			}
+		}
+		
+		System.out.printf("%.0f segundos son %.0f horas, %.0f minutos y %.0f segundos.", segundosAux, horas, minutos, segundos);
+		
+	} 
 }
