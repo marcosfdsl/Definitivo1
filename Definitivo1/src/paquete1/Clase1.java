4360d5ee776479ec33d935804ebf2a4fe18d7511
Alejandro <sastre212324@gmail.com>
2022-12-21 12:16:21 +0100

    f2 1

--- Definitivo1/src/paquete1/Clase1.java
+++ Definitivo1/src/paquete1/Clase1.java
@@ -1,10 +1,22 @@
 package paquete1;
 
+import java.util.Scanner;
+
 public class Clase1 {
 
 	public static void main(String[] args) {
-
-		System.out.println("Hola Mundo");
+		final String ANSI_RESET = "\u001B[0m";
+		final String ANSI_YELLOW = "\u001B[33m";
+		final String ANSI_GREEN = "\u001B[32m";
+		Scanner entrada = new Scanner(System.in);
+		System.out.println(ANSI_YELLOW+"Introduce una palabra: "+ANSI_RESET);
+		String palabra1 = entrada.nextLine();
+		char letras[]= palabra1.toCharArray();
+		StringBuilder palabra = new StringBuilder(palabra1);
+		for(int i=0;i<palabra1.length();i++) {
+		    palabra.insert(i+i,letras[i]);
+		}
+		System.out.println(ANSI_GREEN+palabra);
 
 	}
 
